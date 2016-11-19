bf6a20b25137d476becdc8f907347573423badf0
wangweiquan <Administrator@PC-20160606TZLO>
2016-11-19 17:02:13 +0800

    Initial commit

---
+++ src/main/java/com/elephant/message/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.elephant.message.mapper;
+
+import com.elephant.message.model.User;
+
+/**
+ * Created by zl on 2015/8/27.
+ */
+public interface UserMapper {
+    public User findUserInfo();
+}
